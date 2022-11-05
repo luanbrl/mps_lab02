74f29968db4d419a7158dec8184ab62ba8452dfd
Luan Lima <luanbrl@live.com>
2022-11-05 17:40:38 -0300

    Reimplementando método de imprimir extrato de cliente

--- Locadora.java
+++ Locadora.java
@@ -11,6 +11,27 @@ public class Locadora {
     c1.adicionaAluguel(new Aluguel(new FitaInfantil("Pateta no Planeta dos Macacos"), 10));
     c1.adicionaAluguel(new Aluguel(new FitaInfantil("O Rei Leao"), 30));
 
+    exibirExtratoCliente(c1);
+  }
+
+  public static void exibirExtratoCliente(Cliente cliente) {
+    System.out.println("Registro de Alugueis de " + cliente.getNome());
+    double valorTotal = 0.0;
+    int pontosDeAlugadorFrequente = 0;
+    Iterator alugueis = cliente.getAlugueis().iterator();
+    while(alugueis.hasNext()) {
+      Aluguel aluguel = (Aluguel)alugueis.next();
+      valorTotal += aluguel.getValorAluguel();
+      pontosDeAlugadorFrequente++;
+
+      if(aluguel.getFita() instanceof FitaLancamento && aluguel.getDiasAlugada() > 1) {
+         pontosDeAlugadorFrequente++;
+      }
+
+      System.out.println("\t" + aluguel.getFita().getTitulo() + "\t" + aluguel.getValorAluguel());
+    } 
+    System.out.println("Valor total devido: " + valorTotal);
+    System.out.println("Voce acumulou " + pontosDeAlugadorFrequente + " pontos de alugador frequente");
   }
   
 }
