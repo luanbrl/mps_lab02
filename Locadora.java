7d1e5d02d59d522495b91a71179e494fad260798
Luan Lima <luanbrl@live.com>
2022-11-05 17:39:42 -0300

    Renomeando de Rental para Locadora e ajustando a adição de alugueis

---
+++ Locadora.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Locadora {
+  public static void main(String[] args) {
+    Cliente c1 = new Cliente("Juliana");
+
+    c1.adicionaAluguel(new Aluguel(new FitaNormal("O Exorcista"), 3));
+    c1.adicionaAluguel(new Aluguel(new FitaNormal("Men in Black"), 2));
+    c1.adicionaAluguel(new Aluguel(new FitaLancamento("Jurassic Park III"), 3));
+    c1.adicionaAluguel(new Aluguel(new FitaLancamento("Planeta dos Macacos"), 4));
+    c1.adicionaAluguel(new Aluguel(new FitaInfantil("Pateta no Planeta dos Macacos"), 10));
+    c1.adicionaAluguel(new Aluguel(new FitaInfantil("O Rei Leao"), 30));
+
+  }
+  
+}
