5be60a35c4ee6b544876b77bae5b5e62e66b3381
Luan Lima <luanbrl@live.com>
2022-11-05 17:08:56 -0300

    Renomeando de Rent para Aluguel

---
+++ Aluguel.java
@@ -0,0 +1,18 @@
+public class Aluguel {
+  private Fita fita;
+  private int diasAlugada;
+
+  public Aluguel(Fita fita, int diasAlugada) {
+    this.fita = fita;
+    this.diasAlugada = diasAlugada;
+  }
+
+  public Fita getTape() {
+    return fita;
+  }
+
+  public int getDiasAlugada() {
+    return diasAlugada;
+  }
+}
+
